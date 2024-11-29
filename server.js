772631c2cd64e00005522956c0dda3cf6cb070a3
Pranil Dilip Veer <veerpranil@gmail.com>
2024-11-29 16:44:11 +0530

    Health Api added

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "Server Running", timestamp: new Date().toISOString() });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
