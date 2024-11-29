52878708e0e996be3b329f6769d89aedcfcef4d0
Pranil Dilip Veer <veerpranil@gmail.com>
2024-11-29 16:38:00 +0530

    Updated code with server side rendering

--- README.md
+++ README.md
@@ -7,3 +7,6 @@
 1. Install dependencies: `npm install`
 2. Start the server: `node index.js`
 3. Open `index.html` in a browser.
+
+### Github code link
+- [Github](https://github.com/pranilveer/hello-world-api)
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+require('dotenv').config();
+
+const messages = {
+  English: "Hello world",
+  French: "Bonjour le monde",
+  Hindi: "Namastey sansar",
+};
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/hello", (req, res) => {
+  const { language } = req.query;
+  if (language && messages[language]) {
+    res.json({ msgText: messages[language] });
+  } else {
+    res.status(400).json({ error_message: "The requested language is not supported" });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
